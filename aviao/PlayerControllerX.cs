9b2999b590f5e77913452461fc3bd03a2ffedd0d
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2023-02-14 10:31:08 -0300

    Update PlayerControllerX.cs

--- aviao/PlayerControllerX.cs
+++ aviao/PlayerControllerX.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 10;
-    public float rotationSpeed = 10;
+    public float speed;
+    public float rotationSpeed;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -21,9 +21,9 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
